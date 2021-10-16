efb4cfa668be5234efb2635f132a210d2bec4a94
kadrick <kbk2581553@gmail.com>
2021-10-17 03:55:00 +0900

    add ProjectEuler

---
+++ Math/ProjectEuler/Problem1.c
@@ -0,0 +1,19 @@
+/**
+ * > file: Problem1.c
+ * > author: Kadrick / Bo Gwon Kang (kbk2581553@gmail.com)
+ * > date: 2021-10-17 03:37 
+ * > description: 
+ * @copyright Copyright (c) 2021
+ */
+#include <stdio.h>
+
+int main(void)
+{
+    int ans = 0;
+    for (int i = 0; i < 1000; i++)
+        ans += (i % 3 == 0 || i % 5 == 0) ? i : 0;
+
+    printf("%d\n", ans);
+
+    return 0;
+}
--- README.md
+++ README.md
@@ -2,7 +2,9 @@
 알고리즘, 자료구조, 수학 등을 공부하고 구현해보는 저장소입니다.  
 주로 C/C++ 로 작성될 것입니다.  
   
-It is a storage that studies and implements algorithms, data structures, and mathematics.
+It is a storage that studies and implements algorithms, data structures, and mathematics.  
 It will be mainly written in C/C++.  
 
 ## Contents Table
+* Math
+    * Project Euler
